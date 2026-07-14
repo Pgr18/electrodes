ba50c18cc717bbe223368adc96ee4666b90986be
Pgr18 <paulry@mail.ru>
2026-07-14 20:20:01 +0300

    added manufacturing files

--- kicad_project_processing.bat
+++ kicad_project_processing.bat
@@ -1,10 +1,10 @@
-mkdir output
+mkdir circ_output
 mkdir GERBERS
 mkdir FILES
-kicad-cli sch erc --severity-error --exit-code-violations .\line_electrodes\line_electrodes.kicad_sch 
-kicad-cli sch export pdf -b --output .\output\FILES\sch.pdf .\line_electrodes\line_electrodes.kicad_sch
-kicad-cli sch export bom --output .\output\FILES\bom.csv .\line_electrodes\line_electrodes.kicad_sch 
-kicad-cli pcb drc --schematic-parity --refill-zones --severity-error --exit-code-violations .\line_electrodes\line_electrodes.kicad_pcb
-kicad-cli pcb export gerbers --layers "F.Cu,B.Cu,F.Paste,B.Paste,F.Mask,B.Mask,F.Silkscreen,B.Silkscreen,Edge.Cuts" --use-drill-file-origin --output .\output\GERBERS\ .\line_electrodes\line_electrodes.kicad_pcb
-kicad-cli pcb export drill --excellon-separate-th --drill-origin plot --output .\output\GERBERS\ .\line_electrodes\line_electrodes.kicad_pcb
-kicad-cli pcb export step .\line_electrodes\line_electrodes.kicad_pcb
+kicad-cli sch erc --severity-error --exit-code-violations .\circular_electrodes\circular_electrodes.kicad_sch 
+kicad-cli sch export pdf -b --output .\circ_output\FILES\sch.pdf .\circular_electrodes\circular_electrodes.kicad_sch
+kicad-cli sch export bom --output .\circ_output\FILES\bom.csv .\circular_electrodes\circular_electrodes.kicad_sch 
+kicad-cli pcb drc --schematic-parity --refill-zones --severity-error --exit-code-violations .\circular_electrodes\circular_electrodes.kicad_pcb
+kicad-cli pcb export gerbers --layers "F.Cu,B.Cu,F.Paste,B.Paste,F.Mask,B.Mask,F.Silkscreen,B.Silkscreen,Edge.Cuts" --use-drill-file-origin --output .\circ_output\GERBERS\ .\circular_electrodes\circular_electrodes.kicad_pcb
+kicad-cli pcb export drill --excellon-separate-th --drill-origin plot --output .\circ_output\GERBERS\ .\circular_electrodes\circular_electrodes.kicad_pcb
+kicad-cli pcb export step .\circular_electrodes\circular_electrodes.kicad_pcb
